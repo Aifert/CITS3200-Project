6b17252f7bdd97d1b1d9a5507954ac3fb1771bb2
Jake Pinchin <jakem.pin@gmail.com>
2024-08-21 20:40:51 +0800

    Developed the EntraID integration, fixed routing issues
    Developed code to incorporated the use of EntraID into a login page within the Next,js app.

---
+++ Frontend/cits3200-next-app/pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import AzureADProvider from "next-auth/providers/azure-ad";
+
+export default NextAuth({
+  providers: [
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
+      authorizationUrl: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/oauth2/v2.0/authorize`,
+      tokenUrl: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_ID}/oauth2/v2.0/token`,
+      userinfoUrl: `https://graph.microsoft.com/oidc/userinfo`,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
+});
+
+
+
