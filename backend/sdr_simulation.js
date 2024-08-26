ff94695ca8e3a966c4e7009fdffd87e272931d8e
Aifert Yet <aifertyet0628@gmail.com>
2024-08-26 18:32:29 +0800

    SCRUM-56 chore(docker): add express js code

---
+++ backend/sdr_simulation.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.SDR_PORT || 5001;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/sdr", (req, res) => {
+    const data = req.body;
+    const now = new Date();
+    const timestamp = now.toISOString();
+
+    res.json({
+        timestamp,
+        data
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`SDR server successfully started on port ${PORT}`)
+})
