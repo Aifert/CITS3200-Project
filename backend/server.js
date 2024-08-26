06b4366a7e8f28082d4a97734053c32bc0dff796
Aifert Yet <aifertyet0628@gmail.com>
2024-08-26 18:32:29 +0800

    SCRUM-56 chore(docker): add express js code

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server successfully started on port ${PORT}`);
+});
