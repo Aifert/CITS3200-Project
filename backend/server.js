cd162d1200e06ca6490588a9af20957c558017c4
Aifert Yet <aifertyet0628@gmail.com>
2024-09-04 17:33:30 +0800

    SCRUM-75 feat(docker): clean up

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,16 @@ import { exec } from 'child_process';
 import pg from 'pg' //pg is PostgreSQL
 
 
+const app = express();
+const PORT = process.env.PORT || 9000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const { Client } = pg
 
 const client = new Client({
   user: 'user',
-  host: 'db',
+  host: 'localhost',
   database: 'mydb',
   password: 'password',
   port: 5432,
@@ -26,6 +31,18 @@ client.connect().then(() => {
     client.end()
   });
 });
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+app.use(cors());
+
+app.use(express.json({
+    verify: (req, res, buf) => {
+        req.rawBody = buf;
+    }
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
