4940bf37c071983912cbbddd66f3140e63f6f6c8
Aifert Yet <aifertyet0628@gmail.com>
2024-09-04 17:32:00 +0800

    SCRUM-75 feat(docker): delete verify signature CI/CD

--- backend/server.js
+++ backend/server.js
@@ -27,39 +27,6 @@ client.connect().then(() => {
   });
 });
 
-
-
-const app = express();
-const PORT = process.env.PORT || 9000;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
-app.use(cors());
-
-app.use(express.json({
-    verify: (req, res, buf) => {
-        req.rawBody = buf;
-    }
-}));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-const secret = process.env.WEBHOOK_API_KEY;
-
-function verifySignature(req, res, buf) {
-    const signature = `sha1=${crypto
-        .createHmac('sha1', secret)
-        .update(buf)
-        .digest('hex')}`;
-
-    if (req.headers['x-hub-signature'] !== signature) {
-        return res.status(401).send('Invalid signature.');
-    }
-}
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
