35d4b87320c0b2ead390593ba9664810a9d0f369
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-09-08 20:50:17 +0800

    Reconfigured DB to created during unit test

--- backend/database.test.js
+++ backend/database.test.js
@@ -1,17 +1,38 @@
 //import pg from 'pg' //pg is PostgreSQL//
 const { Client } = require('pg');
+const fs = require('fs');
+
+const initclient = new Client({
+  user: 'user',
+  host: 'localhost',
+  database: 'mydb',
+  password: 'password',
+  port: 5432,
+});
 
 const testclient = new Client({
   user: 'user',
-  host: 'db',
+  host: 'localhost',
   database: 'testdb',
   password: 'password',
   port: 5432,
 });
 
-test("Connect to Server", async () => {
+test("Connect to Init Server", async () => {
+  await initclient.connect();
+  const creation = await initclient.query("CREATE DATABASE testdb");
+  expect(creation.command).toBe("CREATE");
+});
+
+test("Connect to Test Server", async () => {
   await testclient.connect();
-})
+  const initQuery = fs.readFileSync("./webserver/database/init.sql");
+  const initResponse = await testclient.query(initQuery.toString());
+  expect(initResponse.length).toBeGreaterThan(1);
+  const populateQuery = fs.readFileSync("./webserver/database/testpopulate.sql");
+  const populateResponse = await testclient.query(populateQuery.toString());
+  expect(populateResponse.length).toBeGreaterThan(1);
+});
 
 test("Simple Select", async () => {
   const output = await testclient.query('SELECT * FROM "Channels"')
@@ -28,22 +49,17 @@ test("Simple Insertion", async () => {
   expect(initial.rows.length).toBeGreaterThan(0);
   const initialLen = initial.rows.length
 
-  const transact = await testclient.query("START TRANSACTION");
-  expect(transact.command).toBe("START");
 
   await testclient.query('INSERT INTO "Strength" (s_id, c_id, s_sample_time, s_strength) VALUES (3220, 3, 1725547187, -95.73695105828416)');
   const final = await testclient.query("SELECT s_id FROM \"Strength\"");
   const finalLen = final.rows.length
   expect(finalLen-initialLen).toBe(1);
 
-  const roll = await testclient.query("ROLLBACK");
-  expect(roll.command).toBe("ROLLBACK");
-
-  const reset = await testclient.query("SELECT s_id FROM \"Strength\"");
-  expect(reset.rows.length).toBe(initialLen);
-
 })
 
 test("Disconnect from Server", async () => {
   await testclient.end();
+  const drop = await initclient.query("DROP DATABASE testdb");
+  expect(drop.command).toBe("DROP");
+  await initclient.end();
 })
