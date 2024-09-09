1553229664add7a6388743a5cd027fd76e2d7e9d
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-09-09 12:31:40 +0800

    endpoint for active-channels

--- backend/model.js
+++ backend/model.js
@@ -8,7 +8,7 @@ let isConnected = false;
 
 let client = new Client({
         user: process.env.DB_USER || 'user',
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || 'db',
         database: process.env.DB_NAME || 'testdb',
         password: process.env.DB_PASSWORD || 'password',
         port: process.env.DB_PORT || 5432,
@@ -24,7 +24,7 @@ async function connectToDatabase() {
     try {
       client = new Client({
         user: process.env.DB_USER || 'user',
-        host: process.env.DB_HOST || 'localhost',
+        host: process.env.DB_HOST || 'db',
         database: process.env.DB_NAME || 'testdb',
         password: process.env.DB_PASSWORD || 'password',
         port: process.env.DB_PORT || 5432,
@@ -54,6 +54,18 @@ async function recheckConnection() {
   }
 }
 
+function convertToAPIForm(arr) {
+  let output = [];
+  for (const elem of arr) {
+    let info = {};
+    info["channel-id"] = elem["c_id"];
+    info["frequency"] = elem["c_freq"];
+    info["channel-name"] = elem["c_name"];
+    output.push(info);
+  }
+  return output;
+}
+
 async function getAliveChannels() {
   await recheckConnection();
   //Might determine a better method in future but
@@ -61,14 +73,13 @@ async function getAliveChannels() {
   //(From Strength Table because that should constantly be pinged)
   //AND they are not on a device where streaming is currently happening
   //(Unless they are the channel being streamed)
-  const query = `SELECT DISTINCT st.c_id FROM "Strength" AS st INNER JOIN "Channels" AS ch ON st.c_id = ch.c_id 
+  const query = `SELECT DISTINCT ch.c_id, ch.c_freq, ch.c_name FROM "Strength" AS st INNER JOIN "Channels" AS ch ON st.c_id = ch.c_id 
                 WHERE st.s_sample_time > ${Math.floor(new Date().getTime()/1000) - ALIVETIME} AND 
                 ((d_id NOT IN (SELECT d_id FROM "Channels" AS c INNER JOIN "Session_Listeners" AS sl ON c.c_id = sl.c_id)) 
                 OR (st.c_id IN (SELECT c_id FROM "Session_Listeners")))`;
   
   let res = await client.query(query);
-  res.rows = res.rows.map((elem) => elem["c_id"]);
-  return res.rows;
+  return convertToAPIForm(res.rows);
 }
 
 async function getBusyChannels() {
@@ -78,32 +89,25 @@ async function getBusyChannels() {
   //(From Strength Table because that should constantly be pinged)
   //AND they are on a device where streaming is currently happening
   //(and they are not the channel being streamed)
-  const query = `SELECT DISTINCT st.c_id FROM "Strength" AS st INNER JOIN "Channels" AS ch ON st.c_id = ch.c_id 
+  const query = `SELECT DISTINCT ch.c_id, ch.c_freq, ch.c_name FROM "Strength" AS st INNER JOIN "Channels" AS ch ON st.c_id = ch.c_id 
                 WHERE st.s_sample_time > ${Math.floor(new Date().getTime()/1000) - ALIVETIME} AND 
                 ((d_id IN (SELECT d_id FROM "Channels" AS c INNER JOIN "Session_Listeners" AS sl ON c.c_id = sl.c_id)) 
                 AND NOT (st.c_id IN (SELECT c_id FROM "Session_Listeners")))`;
   
   let res = await client.query(query);
-  res.rows = res.rows.map((elem) => elem["c_id"]);
-  return res.rows;
+  return convertToAPIForm(res.rows);
 }
 
 async function getOfflineChannels() {
   await recheckConnection();
   //Might determine a better method in future but
   //Currently "offline" channels are any channels which have not recieved data in the last 15 seconds
-  const query = `SELECT DISTINCT c_id FROM "Channels" 
-                WHERE c_id NOT IN (SELECT c_id FROM "Strength" 
-                WHERE st.s_sample_time > ${Math.floor(new Date().getTime()/1000) - ALIVETIME})`;
+  const query = `SELECT DISTINCT ch.c_id, ch.c_freq, ch.c_name FROM "Channels" AS ch 
+                WHERE ch.c_id NOT IN (SELECT c_id FROM "Strength" 
+                WHERE s_sample_time > ${Math.floor(new Date().getTime()/1000) - ALIVETIME})`;
   
   let res = await client.query(query);
-  res.rows = res.rows.map((elem) => elem["c_id"]);
-  return res.rows;
+  return convertToAPIForm(res.rows);
 }
 
-
-module.exports = {
-  getAliveChannels: getAliveChannels,
-  getBusyChannels: getBusyChannels,
-  getOfflineChannels: getOfflineChannels
-}
+export{ getAliveChannels, getOfflineChannels, getBusyChannels};
