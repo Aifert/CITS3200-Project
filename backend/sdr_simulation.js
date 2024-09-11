751d289ce454c1cc20a3a19f22b2e0355df66a48
Aifert Yet <aifertyet0628@gmail.com>
2024-09-11 21:26:44 +0800

    SCRUM-77 feat(monitor): skeleton code for monitoring

--- backend/sdr_simulation.js
+++ backend/sdr_simulation.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 const PORT = process.env.SDR_PORT || 5000;
+const SDR_URL = "http://host.docker.internal"
 
 app.use(express.json());
 app.use(cors());
@@ -16,6 +18,32 @@ app.post("/sdr", (req, res) => {
         timestamp,
         data
     });
+})
+
+app.get('/stop', async (req, res) => {
+    try{
+        console.log(`${SDR_URL}:5002/stop`);
+        const response = await axios.get(`${SDR_URL}:5002/stop`, {
+            responseType: 'text',
+            headers: {
+                'Accept': 'text/plain'
+            }
+        });
+
+
+        console.log(response)
+    }
+    catch(error){
+        console.log(error);
+    }
+})
+
+app.get('/monitor/:id', async (req, res) => {
+    const frequencyId = req.params.id;
+
+    console.log(frequencyId);
+
+    res.send("hi bozo");
 })
 
 app.listen(PORT, () => {
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
-import pg from 'pg'; // PostgreSQL client
+import pg from 'pg';
+import axios from 'axios'
+
+import { fileURLToPath } from 'url';
 import { getAliveChannels, getOfflineChannels, getBusyChannels} from "./model_utils.js";
+
+const SDR_URL = "http://host.docker.internal"
+// const SDR_URL = "http://0.0.0.0"
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -12,8 +17,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
-const { Client } = pg;
 
 app.use(cors());
 
@@ -25,25 +28,37 @@ app.use(express.json({
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/test', async (req, res) => {
+  axios.get(`${SDR_URL}:5002/stop`, { insecureHTTPParser: true }).then((response) => {
+    res.send(response.data);
+  })
+});
+
+app.get('/monitor-channels', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'monitor.html'))
+})
+
+app.get('/active-channels', async (req, res) => {
+  try{
+    let returnVal = {}
+    returnVal["active"] = await getAliveChannels();
+    returnVal["busy"] = await getBusyChannels();
+    returnVal["offline"] = await getOfflineChannels();
+    res.send(returnVal)
+  }
+  catch(error){
+    res.status(500).send({
+      code: 500,
+      message: "Error occurred while getting channels",
+      error: error.message,
+    })
+  }});
+
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/active-channels', async (req, res) => {
-try{
-  let returnVal = {}
-  returnVal["active"] = await getAliveChannels();
-  returnVal["busy"] = await getBusyChannels();
-  returnVal["offline"] = await getOfflineChannels();
-  res.send(returnVal)
-}
-catch(error){
-  res.status(500).send({
-    code: 500,
-    message: "Error occurred while getting channels",
-    error: error.message,
-  })
-}});
 
 app.listen(PORT, () => {
   console.log(`Server successfully started on port ${PORT}`);
