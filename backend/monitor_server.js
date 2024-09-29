00e768900e874a5ea11fc950f50a6add66a9851b
Aifert Yet <aifertyet0628@gmail.com>
2024-09-29 14:44:26 +0800

    SCRUM-91 chore(middlware): connect server to go sdr server

--- backend/monitor_server.js
+++ backend/monitor_server.js
@@ -1,8 +1,26 @@
 const axios = require('axios');
 
-async function startMonitor(SDR_URL, SDR_PORT, params) {
+async function startMonitorRadio(SDR_URL, SDR_PORT, params) {
   try {
-    const response = await axios.get(`${SDR_URL}:${SDR_PORT}sdr_api/monitor/${params}`,{
+    const response = await axios.get(`${SDR_URL}:${SDR_PORT}/tune/${params}`,{
+      responseType: 'stream',
+      insecureHTTPParser: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+async function startMonitorMP3(SDR_URL, SDR_PORT, params) {
+  const queryParams = Object.entries(params)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join('&');
+  const paramsStr = queryParams ? `?${queryParams}` : '';
+  console.log(`${SDR_URL}:${SDR_PORT}/tune${paramsStr}`);
+  try {
+    const response = await axios.get(`${SDR_URL}:${SDR_PORT}/tune${paramsStr}`,{
       responseType: 'stream',
       insecureHTTPParser: true,
     });
@@ -15,8 +33,8 @@ async function startMonitor(SDR_URL, SDR_PORT, params) {
 
 async function stopMonitor(SDR_URL, SDR_PORT){
     try {
-      console.log(`${SDR_URL}:${SDR_PORT}sdr_api/monitor/stop`);
-      await axios.get(`${SDR_URL}:${SDR_PORT}sdr_api/monitor/stop`, {
+      console.log(`${SDR_URL}:${SDR_PORT}/api/monitor/stop`);
+      await axios.get(`${SDR_URL}:${SDR_PORT}/api/monitor/stop`, {
         insecureHTTPParser: true
       });
     } catch (error) {
@@ -37,7 +55,7 @@ function decideMonitorMode(session_id, channel_id, frequency){
 }
 
 module.exports = {
-  startMonitor,
+  startMonitorMP3,
   stopMonitor,
   decideMonitorMode
 }
