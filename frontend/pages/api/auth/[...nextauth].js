dd8b2b6b1eb5ef87685562e4c20188fa558db5f6
Jake Pinchin <jakem.pin@gmail.com>
2024-10-03 18:15:17 +0800

    Add timeout on authentication

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -25,6 +25,11 @@ export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
+    maxAge: 24 * 60 * 60,
+    updateAge: 12 * 60 * 60,
+  },
+  jwt: {
+    maxAge: 24 * 60 * 60,
   },
   callbacks: {
     async redirect({ url, baseUrl }) {
