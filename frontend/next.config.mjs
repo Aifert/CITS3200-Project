e666fa922ca3a04c9caf597e51f53368877955b4
Aifert Yet <aifert.yet@corpgovrisk.com>
2024-10-10 15:27:50 +0800

    merge main into deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  env: {
+    ...dotenv.config({
+      path: path.resolve(__dirname, '../.env')
+    }).parsed
+  },
+};
 
 export default nextConfig;
