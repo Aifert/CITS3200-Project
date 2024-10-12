de20c7a96b3d29470be8d0066f081480084a356e
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-10-12 23:51:02 +0800

    add device id

--- rasp-pi/radio-transmit/get_go_stream.py
+++ rasp-pi/radio-transmit/get_go_stream.py
@@ -1,20 +1,30 @@
 import requests
 import time
+import hashlib #for hashing the MAC address of SoC, a portion of which will be sent as our soc-id
+import uuid #for receiving MAC address of SoC to hash & send a portion of the digest as our soc-id
 def get_stream(url):
-	s = requests.Session()
+	mac = uuid.getnode()
+	hashed_mac = hashlib.sha256(str(mac).encode()).hexdigest()
+    soc_id = int(hashed_mac[:4], 16)
 
-	with s.get(url, headers=None, stream=True) as resp:
-		lastFrame = ""
-		for line in resp.iter_content(8192):
-			if line:
-				#serverURL = 'https://cits3200-d5bhb7d7gaeqg2b0.australiacentral-01.azurewebsites.net/sdr/pipe_stream'
-				serverURL = 'http://192.168.20.20:9000/sdr/pipe_stream'
-				API_KEY: str = 'aifert-MDUtOVxi1wK_ry9qZyr5OCSTjzu7RUGcvy1_YfHyXSw'
-				headers = {
-				"Content-Type": "application/octet-stream",
-				"Authorization": f"Bearer {API_KEY}",
-				}
-				requests.post(serverURL, data=line, headers=headers, timeout=5, verify=False)
+	s = requests.Session()
+	while True:
+		try:
+			with s.get(url, headers=None, stream=True) as resp:
+				lastFrame = ""
+				for line in resp.iter_content(8192):
+					if line:
+						#serverURL = 'https://cits3200-d5bhb7d7gaeqg2b0.australiacentral-01.azurewebsites.net/sdr/pipe_stream'
+						serverURL = 'http://192.168.20.20:9000/sdr/pipe_stream'
+						API_KEY: str = 'aifert-MDUtOVxi1wK_ry9qZyr5OCSTjzu7RUGcvy1_YfHyXSw'
+						headers = {
+						"Content-Type": "application/octet-stream",
+						"Authorization": f"Bearer {API_KEY}",
+						"Device-Id": soc_id,
+						}
+						requests.post(serverURL, data=line, headers=headers, timeout=5, verify=False)
+		except:
+			time.sleep(10)
 				
 
 url = 'http://localhost:4001/stream'
--- rasp-pi/radio-transmit/tune_go_stream.py
+++ rasp-pi/radio-transmit/tune_go_stream.py
@@ -1,18 +1,21 @@
 import requests
 import time
+#serverURL = 'https://cits3200-d5bhb7d7gaeqg2b0.australiacentral-01.azurewebsites.net/sdr/pipe_stream'
 serverURL = 'http://192.168.20.20:9000/sdr/tune?deviceId=1'
 API_KEY: str = 'aifert-MDUtOVxi1wK_ry9qZyr5OCSTjzu7RUGcvy1_YfHyXSw'
 headers = {
-	"Content-Type": "steam",
+	"Content-Type": "stream",
 	"Authorization": f"Bearer {API_KEY}",
 }
 
+freqStore = 0
+
 while True:
-	print("SENDING")
 	x = requests.get(serverURL, headers=headers).json()
 	if "data" in x:
-		print(x)
 		newFreq = x["data"]["freq"]
-		url = 'http://localhost:4001/tune?file=test-3.mp3'
-		requests.get(url)
-	time.sleep(10)
+		if newFreq != freqStore:
+			url = f'http://localhost:4001/tune?freq={newFreq}'
+			requests.get(url)
+			freqStore=newFreq
+	time.sleep(1)
