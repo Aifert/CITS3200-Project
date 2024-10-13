66cf9d2dda7164c284610cc88329d2858e3e7bf3
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-10-14 02:19:06 +0800

    i am an idiot hahahahahahaha

--- backend/model_utils.js
+++ backend/model_utils.js
@@ -575,8 +575,7 @@ async function santityCheckDatabase(dbName) {
 }
 
 async function getAddressFromChannelId(dbName, c_id) {
-  return ["https://f8a2-2001-8003-907b-7600-4362-c75b-8bcb-57d4.ngrok-free.app/", {freq: 477112500}];
-  /*
+
   try {
      await recheckConnection(dbName);
      let query = `SELECT d.d_address, d.d_port, c.c_freq FROM "devices" AS d JOIN "channels" AS c ON c.d_id = d.d_id WHERE c.c_id = ${c_id}`;
@@ -584,7 +583,7 @@ async function getAddressFromChannelId(dbName, c_id) {
      return [`${res.d_address}/`, {freq: res.c_freq}];
   } catch (error) {
     throw error;
-  }*/
+  }
 }
 
 async function isValidStream(dbName, c_id) {
